19f31802b092619f34229da772b6effdfb5c450a
tenn098 <36396819+tenn098@users.noreply.github.com>
2025-08-12 05:48:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
+app_version = "1.0"
 
 @app.route('/')
-def hello():
-    return "Hello, Docker and CI/CD!"
+def home():
+    return f"Hello from the CI/CD pipeline! App version is {app_version}"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
