79d7cf1443feb3e5b27fe87b2a3355c4f82d3979
tenn098 <36396819+tenn098@users.noreply.github.com>
2025-08-12 09:00:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-app_version = "1.0.2"
+app_version = "1.0.3"
 
 @app.route('/')
 def home():
     return f"Hello from the CI/CD pipeline! App version is {app_version}"
 
 if __name__ == '__main__':
-    app.run(host='0.1.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
